765e6c88f3cfeb7f20a3188f9964a56b9236c627
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 14:01:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaxList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>()
+            { 107, 92, 3, 46, 5096, 666, 85 };
+
+        int maxNumber = numbers.Max();
+
+        Console.WriteLine("The largest number in the list is: " + maxNumber);
+    }
+}
+
